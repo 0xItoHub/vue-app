f7a5351eb20fc1c2e00194a6e02ab2448f346024
E I <154758787+0xItoHub@users.noreply.github.com>
2025-06-29 11:02:37 +0900

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,11 @@
+// postcss.config.cjs （←このファイルはビルドが走る階層に置く）
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    '@tailwindcss/postcss': {
+      // Tailwind 設定ファイルの絶対 or 相対パスを明示
+      // （ビルド開始ディレクトリからの相対パス）
+      config: require('path').resolve(__dirname, './tailwind.config.cjs'),
+    },
     autoprefixer: {},
   },
-}
+};
